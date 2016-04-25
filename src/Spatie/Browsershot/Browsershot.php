4b4f02a08cc38ff97bf0654a49fde760fa8b166e
Marco <zantekk@gmail.com>
2016-04-25 14:08:42 +0200

    fixed phantomJs timeout setting (#24)

--- src/Spatie/Browsershot/Browsershot.php
+++ src/Spatie/Browsershot/Browsershot.php
@@ -275,6 +275,7 @@ class Browsershot
         return "
             var page = require('webpage').create();
             page.settings.javascriptEnabled = true;
+            page.settings.resourceTimeout = ".$this->timeout.";
             page.viewportSize = { width: ".$this->width.($this->height == 0 ? '' : ', height: '.$this->height)." };
             page.open('{$this->url}', function() {
                 if (".($this->backgroundColor ? 'true' : 'false').") {
