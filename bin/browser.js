c10d571e22921743c8f1e21075e3d521ddb58d8f
its <itsgoingd@luzer.sk>
2017-10-09 11:07:47 +0200

    Improved error handling when calling the browser.

--- bin/browser.js
+++ bin/browser.js
@@ -5,20 +5,31 @@ const puppeteer = require('puppeteer')
 const request = JSON.parse(process.argv[2])
 
 let fn = (async () => {
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage()
+    let browser, page
 
-    if (request.options && request.options.userAgent) {
-        await page.setUserAgent(request.options.userAgent)
+    try {
+        browser = await puppeteer.launch()
+        page = await browser.newPage()
+
+        if (request.options && request.options.userAgent) {
+            await page.setUserAgent(request.options.userAgent)
+        }
+
+        if (request.options && request.options.viewport) {
+            await page.setViewport(request.options.viewport)
+        }
+
+        await page.goto(request.url)
+
+        console.log(await page[request.action](request.options))
+
+        await browser.close()
+    } catch (e) {
+        if (browser) {
+            await browser.close()
+        }
+
+        console.error(e)
+        process.exit(1)
     }
-
-    if (request.options && request.options.viewport) {
-        await page.setViewport(request.options.viewport)
-    }
-
-    await page.goto(request.url)
-
-    console.log(await page[request.action](request.options))
-
-    await browser.close()
 })()
