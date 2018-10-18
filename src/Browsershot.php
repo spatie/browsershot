3e46eaf9f9f51d888e787d8cecc4c01734a6da23
zarasargsyan <44243893+zarasargsyan@users.noreply.github.com>
2018-10-18 14:01:49 +0400

    fixing style

--- src/Browsershot.php
+++ src/Browsershot.php
@@ -578,7 +578,7 @@ class Browsershot
         if ($process->isSuccessful()) {
             return rtrim($process->getOutput());
         }
-		$process->clearOutput();
+		 $process->clearOutput();
 
         if ($process->getExitCode() === 2) {
             throw new ElementNotFound($this->additionalOptions['selector']);
